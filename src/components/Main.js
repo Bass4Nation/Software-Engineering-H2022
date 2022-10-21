6078a14804f85722176beafe1026dc18365e6e85
Remi Telle <remi.telle01@gmail.com>
2022-10-21 12:27:12 +0200

    LoggedInUser LocalStorage Access

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,23 @@
 import React from "react";
+import LoggedInUser from "./LoggedInUser";
 
 const Main = () => {
-  
 
 
-  return (<>
 
-  <section>
-    <h2> Velkommen til forsiden</h2>
-    <p> Her så kan vi skrive om siden eller vise noen av biler til utleie. Kanskje?</p>
-  </section>
-  </>
+
+  return (
+    <>
+      <section>
+        <h2> Velkommen til forsiden</h2>
+        <p>
+          {" "}
+          Her så kan vi skrive om siden eller vise noen av biler til utleie.
+          Kanskje?
+        </p>
+        <LoggedInUser test="username"/>
+      </section>
+    </>
   );
 };
 
